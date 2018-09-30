3afe88369408f74a26b13941ce6897d96051bf0a
Kevin Tran <kevin.tran.kt@gmail.com>
2018-09-30 13:43:07 -0700

    Formatted time
    TODO: create config file

--- src/app/widget-template/widget-template.component.ts
+++ src/app/widget-template/widget-template.component.ts
@@ -6,11 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./widget-template.component.scss']
 })
 export class WidgetTemplateComponent implements OnInit {
-  public now: Date = new Date();
+  public formattedDate;
+  public hours;
+  public minutes;
+  public period;
+
+  // TODO: Create config file to hold timeFormat
+  private timeFormat;
 
   constructor() {
+    // TODO: Create config file to hold timeFormat
+    this.timeFormat = '12hr';
+
+    // Update time every second
     setInterval(() => {
-      this.now = new Date();
+      // Get current date
+      let now = new Date();
+      this.formattedDate = now.toLocaleDateString('en-US');
+
+      // Get current hours
+      this.hours = now.getHours();
+
+      if (this.timeFormat == '12hr') {
+        // 12 hr clock mode
+        if (this.hours > 12) {
+          this.period = 'PM';
+          this.hours = this.hours - 12;
+        } else {
+          this.period = 'AM';
+        }
+      } else if (this.timeFormat == '24hr') {
+        // 24 hr clock mode
+        this.period = '';
+      }
+
+      // Get current minutes
+      this.minutes = now.getMinutes();
     }, 1);
   }
 
